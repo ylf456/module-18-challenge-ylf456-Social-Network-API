43de5f3a69d964ad6d316d848f213d17055bcf41
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-02-21 13:28:25 -0800

    debugging getter function

--- models/thought.js
+++ models/thought.js
@@ -10,11 +10,34 @@ const thoughtSchema = new Schema(
       maxlength: 280,
     },
     createAt: {
-      type: Date,
+      type: Date, // type: Date  only accepts date or timestamp
       default: Date.now(),
-      get: function (value) {
-        return moment(value).format("MM-DD-YYYY HH:mm:ss");
+      get: function(value) {
+        console.log(value)
+        const date = new Date(value);
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const MDDYYYY = date.toLocaleDateString("en-US", options);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const formatted_date = `${MDDYYYY} ${hours}:${minutes}:${seconds}`;
+        console.log(formatted_date);
+        return formatted_date;
       },
+      /*
+      set: (value) => {
+        //console.log(value)
+        const date = new Date(value);
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const MDDYYYY = date.toLocaleDateString("en-US", options);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const formatted_date = `${MDDYYYY} ${hours}:${minutes}:${seconds}`;
+        console.log(formatted_date);
+        return formatted_date;
+      },
+      */
     },
     userName: {
       type: String,
@@ -22,16 +45,28 @@ const thoughtSchema = new Schema(
     },
     reactions: [reactionSchema],
   },
-  { collection: 'thought'},
+  { collection: "thought" },
   {
     toJSON: {
       virtuals: true,
       getters: true,
+      setters:true
     },
     id: false,
   }
 );
-
+/*
+thoughtSchema.virtual("formatted_creation_time").get(function () {
+  const date = new Date(Date.now());
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const MDDYYYY = date.toLocaleDateString("en-US", options);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const formatted_date = `${MDDYYYY} ${hours}:${minutes}:${seconds}`;
+  return formatted_date; // Custom format using Moment.js
+});
+*/
 const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
